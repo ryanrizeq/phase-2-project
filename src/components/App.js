d17094b885b1a4cb897eed3599d2e39b06b9570f
Ryan Rizeq <ryanrizeq@gmail.com>
2023-06-30 11:47:42 -0700

    working router for cocktail page

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import NavBar from "./NavBar";
 import { Route, Routes } from "react-router-dom";
+import CocktailPage from "./CocktailPage";
 
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Routes>
+        <Route 
+          path="/list"
+          element={<CocktailPage />}
+        >
+        </Route>
+      </Routes>
       <h1>The CocktaiList</h1>
     </div>
   );
---
+++ src/components/CocktailPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function CocktailPage() {
+    return (
+        <h1>Cocktail Page</h1>
+    )
+}
+
+export default CocktailPage
