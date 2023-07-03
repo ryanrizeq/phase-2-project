b68725726f5a91cab45dd7ffe616284a2b853ebd
Ryan Rizeq <ryanrizeq@gmail.com>
2023-07-03 15:15:00 -0700

    additional styling and content changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,11 @@ import React from "react";
 function Home() {
     return(
         <div>
-            <h1 className="accent">Welcome to CocktaiList!</h1>
+            <h1 className="accent">Welcome to CocktaiList</h1>
             <p>CocktaiList is your personal cocktail résumé. Now you're able to show off your drink reportoire whenever you want.</p>
-            <p>Drink on!</p>
+            <p>Navigate to Cocktail Résumé to see the list of cocktails in your reportoire and show this to others.</p>
+            <p>Navigate to Add to Résumé to add a new cocktail to your reportoire.</p>
+            <p><b>Drink on!</b></p>
         </div>
     )
 }
