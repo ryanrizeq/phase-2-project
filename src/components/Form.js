7b8147e7510ab82b9de052586aa37ef9189be36d
Ryan Rizeq <ryanrizeq@gmail.com>
2023-07-03 10:02:06 -0700

    handle value changes in form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CocktailForm () {
+
+    const [formData, setFormData] = useState({
+        name: "",
+        liqour: "",
+        link: ""
+    })
+
     function onSubmit(e) {
         e.preventDefault()
     }
-    
+
+    function handleNameChange(e) {
+        setFormData({
+            ...formData,
+            name: e.target.value
+        })
+    }
+
+    function handleLiqourChange(e) {
+        setFormData({
+            ...formData,
+            liqour: e.target.value
+        })
+    }
+
+    function handleLinkChange(e) {
+        setFormData({
+            ...formData,
+            link: e.target.value
+        })
+    }
+
     return (
         <div>
             <h1>Add a New Cocktail</h1>
@@ -13,18 +41,24 @@ function CocktailForm () {
                     type="text"
                     name="Cocktail Name"
                     placeholder="Enter Cocktail Name"
+                    value={formData.name}
+                    onChange={handleNameChange}
                 />
                 <br />
                 <input 
                     type="text"
                     name="Cocktail Liqour"
                     placeholder="Enter Cocktail Liqour"
+                    value={formData.liqour}
+                    onChange={handleLiqourChange}
                 />
                 <br />
                 <input 
                     type="text"
                     name="Cocktail Image Link"
                     placeholder="Enter Cocktail Image"
+                    value={formData.link}
+                    onChange={handleLinkChange}
                 />
                 <br />
                 <input 
